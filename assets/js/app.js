bf29d3537290094a3f72a4e0c5897dbe42016fd3
田仲章乃 <>
2019-05-23 12:51:29 +0900

    iTunesAPI完成

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,18 @@
+//カードの雛形
+let cardTemplate = null;
+
 $(function() {
+
+    //card.htmlを読み込む
+    $.get("../../card.html", function(temp) {
+        cardTemplate = $(temp);
+    })
+
     //検索ボタンがクリックされたら
     $("#search-btn").on("click", function() {
+       
+    //検索ワードを取得する
+    let searchWord = $("#search-word").val();
 
     //ajaxを使って、iTunesに検索しに行く
         //データ通信をするところ
@@ -9,14 +21,39 @@ $(function() {
             type: "GET",  //GET送信(値を取得する時)、POST送信(アカウント等を登録する時)
             dataType: "jsonp", //検索結果の形式
             data: {
-                term: "AAA",
+                term: searchWord,
                 country: "jp",
             }
 
         //通信成功した時
         }).done( (data) => {
-            console.log(data);
-        //通信失敗した時
+            //検索結果を消す
+            $("#result").empty();
+
+            //下記処理を繰り返し行う
+            for (item of data.results) {
+                //画像のパス
+                let imgPath = item.artworkUrl100;
+
+                // CDタイトル
+                let collectionName = item.collectionName;
+            
+                // iTunesのリンク
+                let collectionViewUrl = item.collectionViewUrl;
+                
+                // 変数にテンプレートのクローンを入れる
+                let card = cardTemplate.clone();
+
+                // クローンにタイトルなど設定
+                card.find("img").attr("src", imgPath);
+                card.find("h5").text(collectionName);
+                card.find("a").attr("href", collectionViewUrl);
+                
+                // クローンを#resultに追加する
+                $("#result").append(card);
+            }
+
+            //通信失敗した時
         }).fail( (error) => {
             console.log(error);
         })
